9984d0ca585ec81494ab60067fa3c1eb58be79c4
Abraxas-7 <arber.beshaj.02@gmail.com>
2025-02-18 12:01:09 +0100

    Increase maximum image upload limit from 20 to 30 in images route

--- routes/images.js
+++ routes/images.js
@@ -4,6 +4,6 @@ const upload = require("../middlewares/uploads");
 const { store } = require("../controllers/imagesController");
 
 // Rotta per caricare le immagini
-router.post("/:id/upload-images", upload.array("images", 20), store);
+router.post("/:id/upload-images", upload.array("images", 30), store);
 
 module.exports = router;
